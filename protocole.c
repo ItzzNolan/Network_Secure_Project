068d1ec60859d159f1debc1d7ec2564bf6cac3db
ClelieH <clelie.haumesser@yahoo.com>
2026-04-08 17:04:29 +0200

    structure protocole.c

---
+++ protocole.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+
+}message_t;
+
+
+//envoie le json
+message_send(int fd, struct sockaddr_in *dest, char *json_str){
+
+}
+
+//
+message_recv(int fd, char *buffer, int buffer_size, struct sockaddr_in *sender){
+
+}
+
+message_to_json(message_t *msg){
+
+}
+
+json_to_message(char json){
+
+}
+
+int main(int argc, char *argv[]){
+}
