66c08e484f2906940dc2a2e1d6d024eb3418d308
imenmezrigui-sketch <imenmezrigui361@gmail.com>
2026-04-08 14:50:01 +0200

    Create test_c_to_python.py

---
+++ test_c_to_python.py
@@ -0,0 +1,14 @@
+import socket
+
+# Configuration : On écoute sur le port 9998 (défini dans ipc_spec.md)
+UDP_IP = "127.0.0.1"
+UDP_PORT = 9998
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print(f"Python : J'écoute sur le port {UDP_PORT}...")
+
+while True:
+    data, addr = sock.recvfrom(1024) # Attente du message du C
+    print(f"Python : Message reçu du C -> {data.decode('utf-8')}")
