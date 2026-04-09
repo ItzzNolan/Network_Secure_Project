ba425e815f13b3e4a2ca2fb15df98782f123b517
ClelieH <clelie.haumesser@yahoo.com>
2026-04-09 10:08:15 +0200

    message_t

--- protocole.c
+++ protocole.c
@@ -1,12 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct{
+typedef enum {
+    MSG_JOIN,
+    MSG_FULL_STATE,
+    MSG_FULL_STATE_CHUNK,
+    MSG_UPDATE,
+    MSG_DISCONNECT
+} type_message;
 
-}message_t;
+typedef struct {
+    type_message type;
+    int player_id;
+    int entity_id;
+    char *action;    
+    char *data; 
+} message_t;
 
+const char *type_to_string(message_type_t type)
+{
+    switch (type) {
+        case MSG_JOIN: return "JOIN";
+        case MSG_FULL_STATE: return "FULL_STATE";
+        case MSG_UPDATE: return "UPDATE";
+        case MSG_DISCONNECT: return "DISCONNECT";
+        default: return "UNKNOWN";
+    }
+}
 
-//envoie le json
+//envoie le json, découpe si trop gros
 message_send(int fd, struct sockaddr_in *dest, char *json_str){
 
 }
@@ -16,7 +38,11 @@ message_recv(int fd, char *buffer, int buffer_size, struct sockaddr_in *sender){
 
 }
 
-message_to_json(message_t *msg){
+char *message_to_json(message_t *msg){
+    if (!msg) return NULL;
+
+    const char *type_str = type_to_string(msg->type);
+
 
 }
 
