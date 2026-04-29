4e06c4af05fcda8e4291ffbb5e62db9a3186e4b0
ClelieH <clelie.haumesser@yahoo.com>
2026-04-29 10:25:22 +0200

    fonction idfind

--- essai.c
+++ essai.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+int findidsend(char message[], char myid[], int i){
+    
+    char idsend[] = "id_send";
+    for(int j = 0; j<i; j++){
+        if(strstr(message[j], idsend)){
+            printf("%s \n", message[j+2]);
+
+            int idlen = strlen(message[j+2]);
+            char idfind[idlen];
+            strcpy(idfind, message[j+2]);
+        
+            idfind[idlen -1] = '\0';
+            printf("%s \n", idfind);
+            return strcmp(myid,idfind);
+        }else{
+            return 0;
+        }
+   }
+
+}
+
 int main(int argc, char *argv[]){
     /*char chaine[] = "Ceci est un test";
     char recherche[] ="test";
@@ -29,7 +50,7 @@ int main(int argc, char *argv[]){
         i = i+1;
         p= strtok(NULL,sep);
    }
-   char idsend[] = "id_send";
+   /*char idsend[] = "id_send";
    
    for(int j = 0; j<i; j++){
     if(strstr(tab[j], idsend)){
@@ -42,7 +63,9 @@ int main(int argc, char *argv[]){
         idfind[idlen -1] = '\0';
         printf("%s \n", idfind);
     }
-   }
+   }*/
+
+   printf("%d \n", findidsend(tab,"id_michel",i));
 
 
 
--- part2udp.c
+++ part2udp.c
@@ -2,15 +2,42 @@
 #include <stdio.h>
 #include <string.h>
 
+int findidsend(message, myid){
+    
+    char idsend[] = "id_send";
+   
+    for(int j = 0; j<i; j++){
+        if(strstr(message[j], idsend)){
+            printf("%s \n", message[j+2]);
+
+            int idlen = strlen(message[j+2]);
+            char idfind[idlen];
+            strcpy(idfind, message[j+2]);
+        
+            idfind[idlen -1] = '\0';
+            printf("%s \n", idfind);
+            return strcmp(myid,idfind)
+        }else{
+            return 0;
+        }
+   }
+
+}
+
+
 int main(int argc, char *argv[]){
     char request[] = "REQUEST_PROP";
     char answer[] ="ANSWER_PROP";
     char message[]= ;
+
+
 
     if(strstr(message, request)){
         printf("C'est ok \n");
     }else if(strstr(message, answer)){
         printf("C'est pas ok \n");
     }else{
+        NULL;
+    }
 
 }
